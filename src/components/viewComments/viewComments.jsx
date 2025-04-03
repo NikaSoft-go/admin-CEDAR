008d3e9eb0f2adb3af21566a3fc23451f9232e3b
NikaSoft-go <nikasoft73@gmail.com>
2025-04-03 21:05:33 +0000

    comments issue fixed

--- src/components/viewComments/viewComments.jsx
+++ src/components/viewComments/viewComments.jsx
@@ -6,9 +6,9 @@ export default function ViewComments(
     return (
         <div className={"mt-5 flex flex-col"}>
             <p className={"font-bold text-sm mb-3"}>{label}</p>
-            <div style={{height: 60, border: "1px solid #000"}}>
+            <div className={"p-2"} style={{border: "1px solid #000"}}>
                 {comments?.map((elt, index) =>
-                    <p className={"mb-1"} key={index} style={{fontSize: 13}}>{elt?.id}. {elt?.value}</p>
+                    <p className={"mb-1"} key={index} style={{fontSize: 13}}>{elt?.value}</p>
                 )}
             </div>
         </div>
--- src/screens/reports/liftingReport/LiftingPageThree.jsx
+++ src/screens/reports/liftingReport/LiftingPageThree.jsx
@@ -5,7 +5,7 @@ import ViewComments from "../../../components/viewComments/viewComments.jsx";
 const LiftingPageThree = ({ data }) => {
     return (
         <div className="mx-6 mt-5 mb-6">
-            <ViewComments data={data?.comments} label="Comments" />
+            <ViewComments comments={data?.comments} label="Comments" />
             {data?.images?.length > 0 && <ImageSection data={data?.images} />}
             <PdfBottom data={data} />
         </div>
