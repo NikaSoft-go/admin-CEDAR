f01139f525cb98c6f3ef3a7d3fd892fa8fc5e26e
Faisal Issaka <63804699+Faisal-Sey@users.noreply.github.com>
2025-04-05 16:37:40 +0000

    Update liftingInfoView.jsx

--- src/components/liftingInfoView/liftingInfoView.jsx
+++ src/components/liftingInfoView/liftingInfoView.jsx
@@ -42,19 +42,15 @@ export default function LiftingInfoView({ data }) {
                     </thead>
                     <tbody>
                     <tr>
-                        <td className={"text-sm pt-4"} style={{border: 0}}>Within an interval of six months
-                            .......................................................................
+                        <td className={"text-sm pt-4"} style={{border: 0}}>Within an interval of six months {data.lifting_data?.q1_d}
                         </td>
-                        <td className={"text-sm pt-4"} style={{borderBottom: 0, borderLeft: 0}}>Within an interval of 12
-                            months.......................................................................
+                        <td className={"text-sm pt-4"} style={{borderBottom: 0, borderLeft: 0}}>Within an interval of 12 months {data.lifting_data?.q1_e}
                         </td>
                     </tr>
                     <tr>
-                        <td className={"text-sm pt-4"} style={{borderRight: 0}}>In accordance with an examination scheme
-                            .......................................................................
+                        <td className={"text-sm pt-4"} style={{borderRight: 0}}>In accordance with an examination scheme {data.lifting_data?.q1_f}
                         </td>
-                        <td className={"text-sm pt-4"} style={{borderLeft: 0}}>After the occurrence of exceptional
-                            circumstances? .......................................................................
+                        <td className={"text-sm pt-4"} style={{borderLeft: 0}}>After the occurrence of exceptonal circumstances? {data.lifting_data?.q1_g}
                         </td>
                     </tr>
                     </tbody>
